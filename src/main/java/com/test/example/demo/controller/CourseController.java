976251df18a39ad4e2b586c65da3e2c9ff30865e
munaldhakal <dhakal.munal@gmail.com>
2018-08-21 15:31:41 +0545

    Course Controller updated as it was not saved last time.

--- src/main/java/com/test/example/demo/controller/CourseController.java
+++ src/main/java/com/test/example/demo/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,12 @@ public class CourseController {
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<Object> getCourse(){
 		List<CourseResponse> response =courseService.getAllCourse();
+		return new ResponseEntity<Object>(response,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value= "{/id}",method=RequestMethod.GET)
+	public ResponseEntity<Object> getCourseById(@PathVariable Long id){
+		CourseResponse response =courseService.getCourse(id);
 		return new ResponseEntity<Object>(response,HttpStatus.OK);
 	}
 }
